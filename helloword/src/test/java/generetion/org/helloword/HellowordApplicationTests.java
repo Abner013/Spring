9d1d3338810c3d365a33831d145525a16247f5e8
unknown <abner_borges5712@outlook.com>
2021-12-01 17:24:03 -0300

    Hello World

---
+++ helloword/src/test/java/generetion/org/helloword/HellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package generetion.org.helloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
